8e4dee3e4e59724617a3e8915ededa2e98cb33c3
Carlo Sala <carlosalag@protonmail.com>
2022-05-23 14:17:48 +0200

    improve zemu tests

--- tests_zemu/tests/standard.test.ts
+++ tests_zemu/tests/standard.test.ts
@@ -16,7 +16,8 @@
 
 import Zemu, { DEFAULT_START_OPTIONS } from '@zondax/zemu'
 import { newNodleApp } from '@zondax/ledger-substrate'
-import { APP_SEED, models, txBasic, txOther } from './common'
+import { txBalances_transfer, txSession_setKeys, txUtility_batch } from './zemu_blobs'
+import { APP_SEED, models } from './common'
 
 // @ts-ignore
 import ed25519 from 'ed25519-supercop'
@@ -30,7 +31,7 @@ const defaultOptions = {
   X11: false,
 }
 
-jest.setTimeout(60000)
+jest.setTimeout(180000)
 
 beforeAll(async () => {
   await Zemu.checkAndPullImage()
@@ -108,8 +109,7 @@ describe('Standard', function () {
       const respRequest = app.getAddress(0x80000000, 0x80000000, 0x80000000, true)
       // Wait until we are not in the main menu
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
-
-      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-show_address`, 2)
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-show_address`)
 
       const resp = await respRequest
 
@@ -137,8 +137,7 @@ describe('Standard', function () {
       const respRequest = app.getAddress(0x80000000, 0x80000000, 0x80000000, true)
       // Wait until we are not in the main menu
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
-
-      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-show_address_reject`, 3, 2)
+      await sim.navigateAndCompareUntilText('.', `${m.prefix.toLowerCase()}-show_address_reject`, 'REJECT')
 
       const resp = await respRequest
       console.log(resp)
@@ -159,7 +158,7 @@ describe('Standard', function () {
       const pathChange = 0x80000000
       const pathIndex = 0x80000000
 
-      const txBlob = Buffer.from(txBasic, 'hex')
+      const txBlob = Buffer.from(txBalances_transfer, 'hex')
 
       const responseAddr = await app.getAddress(pathAccount, pathChange, pathIndex)
       const pubKey = Buffer.from(responseAddr.pubKey, 'hex')
@@ -168,8 +167,7 @@ describe('Standard', function () {
       const signatureRequest = app.sign(pathAccount, pathChange, pathIndex, txBlob)
       // Wait until we are not in the main menu
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
-
-      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-sign_basic_normal`, 4)
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-sign_basic_normal`)
 
       const signatureResponse = await signatureRequest
       console.log(signatureResponse)
@@ -205,7 +203,7 @@ describe('Standard', function () {
       await sim.clickBoth()
       await sim.clickLeft()
 
-      const txBlob = Buffer.from(txBasic, 'hex')
+      const txBlob = Buffer.from(txBalances_transfer, 'hex')
 
       const responseAddr = await app.getAddress(pathAccount, pathChange, pathIndex)
       const pubKey = Buffer.from(responseAddr.pubKey, 'hex')
@@ -215,8 +213,7 @@ describe('Standard', function () {
 
       // Wait until we are not in the main menu
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
-
-      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-sign_basic_expert`, 10)
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-sign_basic_expert`)
 
       const signatureResponse = await signatureRequest
       console.log(signatureResponse)
@@ -238,7 +235,7 @@ describe('Standard', function () {
     }
   })
 
-  test.each(models)('sign other', async function (m) {
+  test.each(models)('set keys', async function (m) {
     const sim = new Zemu(m.path)
     try {
       await sim.start({ ...defaultOptions, model: m.name })
@@ -247,7 +244,7 @@ describe('Standard', function () {
       const pathChange = 0x80000000
       const pathIndex = 0x80000000
 
-      const txBlob = Buffer.from(txOther, 'hex')
+      const txBlob = Buffer.from(txSession_setKeys, 'hex')
 
       const responseAddr = await app.getAddress(pathAccount, pathChange, pathIndex)
       const pubKey = Buffer.from(responseAddr.pubKey, 'hex')
@@ -256,8 +253,47 @@ describe('Standard', function () {
       const signatureRequest = app.sign(pathAccount, pathChange, pathIndex, txBlob)
       // Wait until we are not in the main menu
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-set-keys`)
 
-      await sim.compareSnapshotsAndAccept('.', `${m.prefix.toLowerCase()}-sign_large_other`, m.name === 'nanos' ? 6 : 7)
+      const signatureResponse = await signatureRequest
+      console.log(signatureResponse)
+
+      expect(signatureResponse.return_code).toEqual(0x9000)
+      expect(signatureResponse.error_message).toEqual('No errors')
+
+      // Now verify the signature
+      let prehash = txBlob
+      if (txBlob.length > 256) {
+        const context = blake2bInit(32)
+        blake2bUpdate(context, txBlob)
+        prehash = Buffer.from(blake2bFinal(context))
+      }
+      const valid = ed25519.verify(signatureResponse.signature.slice(1), prehash, pubKey)
+      expect(valid).toEqual(true)
+    } finally {
+      await sim.close()
+    }
+  })
+
+  test.each(models)('utility batch', async function (m) {
+    const sim = new Zemu(m.path)
+    try {
+      await sim.start({ ...defaultOptions, model: m.name })
+      const app = newNodleApp(sim.getTransport())
+      const pathAccount = 0x80000000
+      const pathChange = 0x80000000
+      const pathIndex = 0x80000000
+
+      const txBlob = Buffer.from(txUtility_batch, 'hex')
+
+      const responseAddr = await app.getAddress(pathAccount, pathChange, pathIndex)
+      const pubKey = Buffer.from(responseAddr.pubKey, 'hex')
+
+      // do not wait here.. we need to navigate
+      const signatureRequest = app.sign(pathAccount, pathChange, pathIndex, txBlob)
+      // Wait until we are not in the main menu
+      await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-utility-batch`)
 
       const signatureResponse = await signatureRequest
       console.log(signatureResponse)
